8757a8ba559f4c25e9237635fc5aa098655c5a0c
Aladdin-Elshamy <122921299+Aladdin-Elshamy@users.noreply.github.com>
2023-07-20 17:32:13 +0300

    Add getInfo function

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let perviousNum = 0;
 let tries = 0;
 let message = {
-    study : ["Find your “thing” – like Stephen Hawking", "Stay consistent in your studying", "Make each day a little better than before"],
-    dream : ["Let your actions be louder than your words and your dreams bigger than your fears." ,"Always nurture your inner childhood dreams, life will become beautiful" ,"The only thing that will stop you from fulfilling your dreams is you."],
-    success : ["However difficult life may seem, there is always something you can do and succeed at.", "People begin to become successful the minute they decide to be.", "It does not matter how slowly you go as long as you do not stop."]
+    study : ['Find your “thing” – like Stephen Hawking', 'Stay consistent in your studying', 'Make each day a little better than before'],
+    dream : ['Let your actions be louder than your words and your dreams bigger than your fears.' ,'Always nurture your inner childhood dreams, life will become beautiful' ,'The only thing that will stop you from fulfilling your dreams is you.'],
+    success : ['However difficult life may seem, there is always something you can do and succeed at.', 'People begin to become successful the minute they decide to be.', 'It does not matter how slowly you go as long as you do not stop.']
 }
+const getInfo = () => {
+    console.log('Welcome to our inspirational message generator');
+    const prompt = require('prompt-sync')();
+    const name = prompt('What is your name? ');
+    console.log(`Hey there ${name}`);
+}
+getInfo();
